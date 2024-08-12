cd8760d7056fe3dd9208d7d1ef8cf1478637a265
MaximilianNoethe <maximilian.noethe@noseryoung.com>
2024-08-12 14:11:58 +0200

    Initialize backend

---
+++ grocerie/src/test/java/ch/noseryoung/grocerie/GrocerieApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.noseryoung.grocerie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrocerieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
