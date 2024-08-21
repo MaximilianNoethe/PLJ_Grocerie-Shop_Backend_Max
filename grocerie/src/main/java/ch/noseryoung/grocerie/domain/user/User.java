1151d70967cc69238ff3a968072e98e909cd5b47
MaximilianNoethe <maximilian.noethe@noseryoung.com>
2024-08-21 10:40:08 +0200

    Update user fields

--- grocerie/src/main/java/ch/noseryoung/grocerie/domain/user/User.java
+++ grocerie/src/main/java/ch/noseryoung/grocerie/domain/user/User.java
@@ -18,12 +18,6 @@ public class User {
     @Column(name = "first_name")
     private String firstName;
 
-    @Column(name = "last_name")
-    private String lastName;
-
-    @Column(name = "email")
-    private String email;
-
     @Column(name = "password")
     private String password;
 
